6b0f08ecfa8ddaf2186bfd1f25439887a3c88f2a
RandomPass Developer <no-reply@example.com>
2025-05-09 18:47:43 +0800

    fix: 修正部署配置和资源路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,23 @@
- 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: '/luxejapan-public/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
+}) 
